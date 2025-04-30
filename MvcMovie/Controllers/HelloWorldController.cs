ba1d2ffeec5fa9079213d3b1cedc7070f2f7c81d
limfh <roznova69@gmail.com>
2025-04-30 15:53:15 +0300

    commit

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers;
+
+public class HelloWorldController : Controller
+{
+    // 
+    // GET: /HelloWorld/
+    public string Index()
+    {
+        return "privetick ";
+    }
+    // 
+    // GET: /HelloWorld/Welcome/ 
+ 
+    public string Welcome(string name, int numTimes = 100000)
+    {
+        return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+    }
+}
