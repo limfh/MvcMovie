4fba66d8cff87fc67727172d982acde3ca41aa21
limfh <roznova69@gmail.com>
2025-04-30 16:43:35 +0300

    commit4

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -5,17 +5,14 @@ namespace MvcMovie.Controllers;
 
 public class HelloWorldController : Controller
 {
-    // 
-    // GET: /HelloWorld/
-    public string Index()
+    public IActionResult Index()
     {
-        return "privetick ";
+        return View();
     }
-    // 
-    // GET: /HelloWorld/Welcome/ 
- 
-    public string Welcome(string name, int numTimes = 100000)
+    public IActionResult Welcome(string name, int numTimes = 1)
     {
-        return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        ViewData["Message"] = "Hello "+ name;
+        ViewData["NumTimes"] = numTimes;
+        return View();
     }
 }
